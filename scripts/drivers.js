1b19aa25feb380526d7b7419c4aa94ef2700a72b
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-02-20 12:58:36 +0100

    Ajustes a revisar

--- scripts/drivers.js
+++ scripts/drivers.js
@@ -29,7 +29,7 @@ const countryMap = {
 
 const container_piloto = document.getElementById("drivers-container")
 
-fetch("https://api.openf1.org/v1/drivers?session_key=9839")
+fetch("https://api.openf1.org/v1/drivers?session_key=9837")
     .then((response) => response.json())
     .then((jsonContent) =>
         jsonContent.forEach(driver => {
